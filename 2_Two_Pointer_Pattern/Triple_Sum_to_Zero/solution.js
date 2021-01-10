66479e39268f1cc384cfd2de6f34b5716eea85f9
Ralph Plumley <ralphplumley@gmail.com>
2021-01-10 11:21:18 -0500

    Review Triple Sum to Zero

--- 2_Two_Pointer_Pattern/Triple_Sum_to_Zero/solution.js
+++ 2_Two_Pointer_Pattern/Triple_Sum_to_Zero/solution.js
@@ -1,38 +1,26 @@
-const search_triplets = function (arr) {
-    let triplets = []
+const search_triplets = function(arr) {
+    // sort
+    arr.sort((a,b) => a-b)
 
-    // sort the array - O(log n)
-    arr.sort(function (a, b) {
-        return a - b
-    })
+    let triplets = [];
+    for (let x = 0; x < arr.length; x++) {
+        let y = x + 1, z = arr.length - 1
+        while (y < z) {
+            let target_sum = 0 - arr[x]
+            let yz_sum = arr[y] + arr[z]
 
-    // Start in the 2nd slot
-    // [ ,x, , , ] 
-    for (let anchor = 1; anchor < arr.length - 1; anchor++) {
-        if (arr[anchor] === arr[anchor - 1]) continue
-        let ptr_left = anchor - 1
-        let ptr_right = anchor + 1
-
-        while (!(ptr_left < 0) && !(ptr_right > arr.length - 1)) {
-            let a = arr[ptr_left]
-            let b = arr[anchor]
-            let c = arr[ptr_right]
-            let sum = a + b + c
-
-            if (sum === 0) {
-                triplets.push([a, b, c])
-                while (arr[ptr_right] === c) {
-                    ptr_right += 1
-                }
-            } else if (sum < 0) {
-                ptr_right += 1
-            } else if (sum > 0) {
-                ptr_left -= 1
+            if (yz_sum === target_sum) { // found a triple
+                let triple = [arr[x], arr[y], arr[z]]
+                triplets.push(triple)
+                y += 1
+                z -= 1
+            } else if (yz_sum > target_sum) {
+                z -= 1
+            } else {
+                y += 1
             }
         }
     }
-
-    return triplets
+    return triplets;
 }
-
 module.exports = search_triplets
