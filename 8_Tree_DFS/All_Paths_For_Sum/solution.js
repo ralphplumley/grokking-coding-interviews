decc66df65e6f94bc3d69ce839778658991b8bf8
Ralph Plumley <ralphplumley@gmail.com>
2021-02-10 18:00:45 -0500

    retry All Paths for Sum

--- 8_Tree_DFS/All_Paths_For_Sum/solution.js
+++ 8_Tree_DFS/All_Paths_For_Sum/solution.js
@@ -1,53 +1,50 @@
 class TreeNode {
-    constructor(val, left = null, right = null) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-}
+};
 
 
-function find_paths(root, sum) {
+const find_paths = function(root, sum) {
     let allPaths = [];
-    let queue = []
-    find_paths_recursive(root, sum, queue, allPaths);
+    let current_path = []
+    recursive(root, sum, current_path, allPaths)
+    // allPaths.forEach((arr) => {
+    //     console.log(arr)
+    // })
     return allPaths;
-}
+};
 
+const recursive = function(node, sum, current_path, allPaths) {
+    if (!node) return false
 
-function find_paths_recursive(currentNode, sum, currentPath, allPaths) {
-    if (currentNode === null) {
-        return;
-    }
-
-    // add the current node to the path
-    currentPath.push(currentNode.val);
+    // add current node to path
+    current_path.push(node.value)
 
     // if the current node is a leaf and its value is equal to sum, save the current path
-    if (currentNode.val === sum && currentNode.left === null && currentNode.right === null) {
-        allPaths.push(currentPath);
+    if (!node.left && !node.right && node.value === sum) {
+        allPaths.push(Array.from(current_path))
     } else {
-        // traverse the left sub-tree
-        find_paths_recursive(currentNode.left, sum - currentNode.val, currentPath, allPaths);
-        // traverse the right sub-tree
-        find_paths_recursive(currentNode.right, sum - currentNode.val, currentPath, allPaths);
+        // traverse the left tree
+        recursive(node.left, sum - node.value, current_path, allPaths)
+        // traverse the right tree
+        recursive(node.right, sum - node.value, current_path, allPaths)
     }
-    // remove the current node from the path to backtrack,
-    // we need to remove the current node while we are going up the recursive call stack.
-    currentPath.pop();
+
+    // If we get here, it means we are at a dead end, and this path does not have the sum.
+    // So, remove the current node from the path to backtrack,
+    // we need to remove the current node while we are going up the recursive call stack
+    current_path.pop()
 }
 
 
-const root = new TreeNode(12);
-root.left = new TreeNode(7);
-root.right = new TreeNode(1);
-root.left.left = new TreeNode(4);
-root.right.left = new TreeNode(10);
-root.right.right = new TreeNode(5);
-let sum = 23,
-    result = find_paths(root, sum);
-
-process.stdout.write(`Tree paths with sum ${sum}: `);
-for (i = 0; i < result.length; i++) {
-    process.stdout.write(`[${result[i]}] `);
-}
+var root = new TreeNode(12)
+root.left = new TreeNode(7)
+root.right = new TreeNode(1)
+root.left.left = new TreeNode(4)
+root.right.left = new TreeNode(10)
+root.right.right = new TreeNode(5)
+sum = 23
+console.log(`Tree paths with sum: ${sum}: ${find_paths(root, sum)}`)
